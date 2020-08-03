0cdd6f2d85220287eb97811da3e0959b764552c6
Saurav Suman <sauravsuman001@hotmail.com>
2020-08-03 20:47:14 +0530

    Implemented RxJava with retrofit2 for request and response.

--- app/src/main/java/com/demoapps/openweather/MainActivity.kt
+++ app/src/main/java/com/demoapps/openweather/MainActivity.kt
@@ -4,39 +4,57 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.demoapps.openweather.model.OpenWeatherResponse
 import com.demoapps.openweather.utils.RetrofitClient
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private val baseUrl = "https://api.openweathermap.org/"
+    private val KEY = "6ef89489ba6ce1fddb4d5041a0727a03"
+    private val LOCATION = "Bangalore"
+    private var compositeDisposable: CompositeDisposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         init()
+
     }
 
     private fun init() {
         getWeaterButton.setOnClickListener {
-            val apiServices = RetrofitClient.getApiClient(baseUrl)
-            apiServices?.getCurrentWeatherData("6ef89489ba6ce1fddb4d5041a0727a03", "bangalore")
-                ?.enqueue(object : Callback<OpenWeatherResponse> {
-                    override fun onFailure(call: Call<OpenWeatherResponse>, t: Throwable) {
-                        getWeaterButton.text = "Api Failed"
-                    }
-
-                    override fun onResponse(
-                        call: Call<OpenWeatherResponse>,
-                        response: Response<OpenWeatherResponse>
-                    ) {
-                        val responseBody = response.body()
-                        if (responseBody != null) {
-                            getWeaterButton.text = responseBody.weatherDetails.currentTemperature
-                        }
-                    }
-                })
+            throughRxJava()
         }
+    }
+
+    private fun throughRxJava() {
+        compositeDisposable = CompositeDisposable()
+        val apiServices = RetrofitClient.getApiClient(baseUrl)
+        if (apiServices != null) {
+            compositeDisposable?.add(apiServices.getCurrentWeatherData(KEY, LOCATION)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe({ response ->
+                    handleSuccess(response)
+                }, { error ->
+                    handleError(error)
+                })
+            )
+        }
+    }
+
+    private fun handleSuccess(response: OpenWeatherResponse) {
+        getWeaterButton.text = response.weatherDetails.currentTemperature
+
+    }
+
+    private fun handleError(error: Throwable) {
+        getWeaterButton.text = "Api Failed"
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable?.clear()
     }
 }
--- app/src/main/java/com/demoapps/openweather/interfaces/ApiServices.kt
+++ app/src/main/java/com/demoapps/openweather/interfaces/ApiServices.kt
@@ -1,11 +1,14 @@
 package com.demoapps.openweather.interfaces
 
 import com.demoapps.openweather.model.OpenWeatherResponse
-import retrofit2.Call
+import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiServices {
     @GET("data/2.5/weather?")
-    fun getCurrentWeatherData(@Query("APPID") app_id: String, @Query("q") location: String): Call<OpenWeatherResponse>
+    fun getCurrentWeatherData(
+        @Query("APPID") app_id: String,
+        @Query("q") location: String
+    ): Observable<OpenWeatherResponse>
 }
